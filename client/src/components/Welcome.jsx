d416d428b925eb424d1b6143d5ea4efbdc5cdc87
sunzhqr <sanzhar_myrzash@mail.ru>
2024-02-29 12:38:11 +0600

    Sanzhar's part

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,10 @@
+import React, { useContext } from "react";
 import {AiFillPlayCircle }from 'react-icons/ai';
 import {SiEthereum} from 'react-icons/si';
 import {BsInfoCircle} from 'react-icons/bs';
 
- import {Loader} from './'
+import { TransactionContext } from "../context/TransactionContext.jsx";
+import {Loader} from './'
 
 
  const Input = ({ placeholder, name, type, value, handleChange }) => (
@@ -19,14 +21,18 @@ import {BsInfoCircle} from 'react-icons/bs';
   const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
 
 const Welcome = () => {
+    const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext);
 
-    const connectWallet = () => {
 
-    }
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
 
-const handleSubmit = () => {
+        e.preventDefault();
 
-}
+        if (!addressTo || !amount || !keyword || !message) return;
+
+        sendTransaction();
+    };
 
     
 
@@ -41,22 +47,24 @@ const handleSubmit = () => {
             <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
                 Explore the crypto world.Buy and sell cryptocurrencies easily on Krypto. 
             </p>
+              {!currentAccount && (
+                  <button type='button' onClick={connectWallet}
+                          className='flex flex-row justify-center items-center my-5 bg-[#2952e3] rounded-full cursor-pointer p-3 hover:bg-[#2546bd]'>
+                      <p className='text-white text-base font-semibold'>
+                          Connect Wallet
+                      </p>
 
-            <button type='button' onClick={connectWallet} className='flex flex-row justify-center items-center my-5 bg-[#2952e3] rounded-full cursor-pointer p-3 hover:bg-[#2546bd]'>
-                <p className='text-white text-base font-semibold'>
-                    Connect Wallet
-                    </p>
-                    
-            </button>
+                  </button>
+              )}
 
-            <div className='grid sm:grid-cols-3 grid-cols-2 mt-10 w-full'>
-         <div className={`rounded-tl-2xl ${commonStyles}`}>
-           Reliability
-         </div>
-         <div className={commonStyles}>Security</div>
-            <div className={`sm:rounded-tr-2xl ${commonStyles}`}>
-              Ethereum
-            </div>
+              <div className='grid sm:grid-cols-3 grid-cols-2 mt-10 w-full'>
+                  <div className={`rounded-tl-2xl ${commonStyles}`}>
+                      Reliability
+                  </div>
+                  <div className={commonStyles}>Security</div>
+                  <div className={`sm:rounded-tr-2xl ${commonStyles}`}>
+                      Ethereum
+                  </div>
             <div className={`sm:rounded-bl-2xl ${commonStyles}`}>
               Web 3.0
             </div>
@@ -87,10 +95,10 @@ const handleSubmit = () => {
             </div>
           </div>
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
-            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
-            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
-            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
+import abi from './Transactions.json';
 
-
-
+export const conrtactABI = abi.abi;
 export const contractAddress = '0x143a11Ba45Da7D74DeC354EaFBF3a669C2461075';
